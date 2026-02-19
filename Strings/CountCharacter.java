c1c8695e4cc06d06c5ee0445c64f019289dba1ab
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-02-19 16:01:23 +0530

    Strings

---
+++ Strings/CountCharacter.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class CountCharacter {
+  public static void main(String[] args){
+    String str = "Hello length";
+    int count = str.length();
+    System.out.println("String has number of characters : "+count);
+  }
+}
