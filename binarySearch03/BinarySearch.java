ff66462ba18b556b9b4aa4c46a9c63e6f41193eb
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-02-20 00:23:31 +0530

    Binary Search Programs

---
+++ binarySearch03/BinarySearch.java
@@ -0,0 +1,37 @@
+package binarySearch03;
+import java.util.*;
+public class BinarySearch {
+
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter size of array - ");
+    int n = sc.nextInt();
+    int[] a = new int[n];
+
+    System.out.println("Enter elements of array : ");
+    for(int i = 0; i < n ; i++){
+      a[i] = sc.nextInt();
+    }
+    
+    System.out.println("enter your target-");
+    int t = sc.nextInt();
+    int st = 0;
+    int end = n-1;
+    while(st<=end){
+      int mid = st+(end-st)/2;
+      if(a[mid]==t){
+        System.out.println(a[mid]+" found at index "+mid);
+        break;
+      }else if(t<a[mid]){
+        end = mid - 1;
+      }else{
+        st = mid + 1;
+      }
+    }
+    if(st>end){
+      System.out.println("element does not found");
+    } 
+
+    sc.close();
+  }
+}
---
+++ binarySearch03/RecursiveBinary.java
@@ -0,0 +1,25 @@
+package binarySearch03;
+
+public class RecursiveBinary {
+  
+  static boolean recur(int[] a, int t, int st, int end){
+    if(st>end){
+      return false;
+    }
+    
+      int mid = st + (end-st)/2;
+      if(a[mid]==t){
+        return true;
+      }else if(t < a[mid]){
+        return recur(a,t,st,mid-1);
+      }else{
+        return recur(a,t,mid+1,end);
+      }
+    }
+  
+  public static void main(String[] args){
+    int[] a = {2,3,4,5,6};
+    int t = 6, st=0, end = a.length - 1;
+    System.out.println(recur(a,t,st,end));
+  }
+}
