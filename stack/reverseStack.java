12e6ec96f0595679158a79a781bfb45d3233576b
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-03-08 22:33:51 +0530

    push  elements from one stack to another

---
+++ stack/reverseStack.java
@@ -0,0 +1,24 @@
+package stack;
+import java.util.*;
+
+public class reverseStack {
+  public static void main(String[] args){
+    Stack<Integer> st = new Stack<>();
+    st.push(3);
+    st.push(4);
+    st.push(6);
+    st.push(2);
+    System.out.println(st);
+
+
+     //reverse stack element
+    Stack<Integer> reverseStack = new Stack<>();
+    while(st.size()>0){
+      reverseStack.push(st.pop());
+    }
+    System.out.println(reverseStack);
+
+    //reversing again to maintain same order as st into result stack
+    
+  }
+}
