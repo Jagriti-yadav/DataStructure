ce07a3aeadcc91bf229ed79b13b756c6a489754f
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-03-21 12:26:40 +0530

    max sum subarray

--- slidingwindow08/MaxSumSubArray.java
+++ slidingwindow08/MaxSumSubArray.java
@@ -1,23 +1,32 @@
 package slidingwindow08;
+import java.util.*;
 
-public class MaxSumSubArray {
-  static int SumSubArray(int[] a,int k){
-    int sum = 0;
-    for(int i = 0; i < k ; i++){
-      sum += a[i];
+public class MaxSumSubArray{
+  static int SumSubArray(int[] arr,int k){
+    int Currentsum = 0;
+    int n = arr.length;
+    for(int i = 0 ; i < k;i++){
+      Currentsum += arr[i];
     }
-    int max = sum;
-    for(int i = 1; i<=a.length-k;i++){
-      sum = sum - a[i-1] + a[i+k-1];
-      if(sum>max){
-        max = sum;
+    int max = Currentsum;
+
+    //sliding window
+    for(int i = 1;i<=n-k;i++){
+      Currentsum = Currentsum - arr[i-1] + arr[i+k-1];
+      if(max<Currentsum){
+        max = Currentsum;
       }
     }
     return max;
   }
   public static void main(String[] args){
-    int[] arr = {3,8,2,5,7,6,12};
-    int k = 3;
-    System.out.println(SumSubArray(arr,k));
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter window size -");
+    int[] arr = {3,8,2,1,7,0,5,4};
+    int k = sc.nextInt();
+    int max = SumSubArray(arr,k);
+    System.out.println(max);
+    sc.close();
   }
+  
 }
